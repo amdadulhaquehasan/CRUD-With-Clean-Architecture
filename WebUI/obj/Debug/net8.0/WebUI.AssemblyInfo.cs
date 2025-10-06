211ff663e4b38f827d72c81ec96705864561456a
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-10-07 01:24:17 +0600

    Add Debugger and csproj files

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8f12836c1243a49cbf5c177b629e3a94fa5759")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
