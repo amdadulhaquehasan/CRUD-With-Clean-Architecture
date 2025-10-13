05c4ace0e99455983bbee9c674a31cb93757e7e0
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-10-13 20:47:41 +0600

    Add AccountService File

---
+++ Application/Services/AccountService.cs
@@ -0,0 +1,158 @@
+using System.Net.Http.Json;
+using Application.DTOs.Request.Account;
+using Application.DTOs.Response;
+using Application.DTOs.Response.Account;
+using Application.Extensions;
+
+namespace Application.Services;
+
+public class AccountService(HttpClientService httpClientService) : IAccountService
+{
+    public async Task<LoginResponse> LoginAccountAsync(LoginDTO model)
+    {
+        try
+        {
+            var publicClient = httpClientService.GetPublicClient();
+            var response = await publicClient.PostAsJsonAsync(Constant.LoginRoute, model);
+            string error = CheckResponseStatus(response);
+            if (!string.IsNullOrEmpty(error))
+                return new LoginResponse(Flag: false, Message: error);
+
+            var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new LoginResponse(Flag: false, Message: ex.Message);
+        }
+    }
+
+    public async Task<GeneralResponse> CreateAccountAsync(CreateAccountDTO model)
+    {
+        try
+        {
+            var publicClient = httpClientService.GetPublicClient();
+            var response = await publicClient.PostAsJsonAsync(Constant.RegisterRoute, model);
+            var error = CheckResponseStatus(response);
+            if (!string.IsNullOrEmpty(error))
+                return new GeneralResponse(Flag: false, Message: error);
+
+            var result = await response.Content.ReadFromJsonAsync<GeneralResponse>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new GeneralResponse(Flag: false, Message: ex.Message);
+        }
+    }
+
+    public static string CheckResponseStatus(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+            return $"Sorry unknown error occured. {Environment.NewLine}Error Description: {Environment.NewLine}Status Code: {response.StatusCode}{Environment.NewLine}Reason Phrase: {response.ReasonPhrase}";
+        else
+            return null;
+    }
+
+    public async Task CreateAdmin()
+    {
+        try
+        {
+            var client = httpClientService.GetPublicClient();
+            await client.PostAsync(Constant.CreateAdminRoute, null);
+        }
+        catch{ }
+    }
+
+    public async Task<IEnumerable<GetRoleDTO>> GetRolesAsync()
+    {
+        try
+        {
+            var privateClient = await httpClientService.GetPrivateClient();
+            var response = await privateClient.GetAsync(Constant.GetRolesRoute);
+            string error = CheckResponseStatus(response);
+            if (!string.IsNullOrEmpty(error))
+                throw new Exception(error);
+            
+            var result = await response.Content.ReadFromJsonAsync<IEnumerable<GetRoleDTO>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    /*public IEnumerable<GetRoleDTO> GEtDefaultRoles()
+    {
+        var list = new List<GetRoleDTO>();
+        list?.Clear();
+        list.Add(new GetRoleDTO(1.ToString(), Constant.Role.Admin));
+        list.Add(new GetRoleDTO(2.ToString(), Constant.Role.User));
+        return list;
+    }*/
+
+    public Task<GeneralResponse> CreateRoleAsync(CreateRoleDTO model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<GetUsersWithRolesResponseDTO>> GetUsersWithRolesAsync()
+    {
+        try
+        {
+            var privateClient = await httpClientService.GetPrivateClient();
+            var response = await privateClient.GetAsync(Constant.GetUserWithRolesRoute);
+            string error = CheckResponseStatus(response);
+            if (!string.IsNullOrEmpty(error))
+                throw new Exception(error);
+            
+            var result = await response.Content.ReadFromJsonAsync<IEnumerable<GetUsersWithRolesResponseDTO>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<GeneralResponse> ChangeUserRoleAsync(ChangeUserRoleRequestDTO model)
+    {
+        try
+        {
+            var publicClient = httpClientService.GetPublicClient();
+            var response = await publicClient.PostAsJsonAsync(Constant.ChangeUserRoleRoute, model);
+            string error = CheckResponseStatus(response);
+            if (!string.IsNullOrEmpty(error))
+                return new GeneralResponse(false,error);
+            
+            var result = await response.Content.ReadFromJsonAsync<GeneralResponse>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new GeneralResponse(false, ex.Message);
+        }
+        
+        
+    }
+
+    public async Task<LoginResponse> RefreshTokenAsync(RefreshTokenDTO model)
+    {
+        try
+        {
+            var publicClient = httpClientService.GetPublicClient();
+            var response = await publicClient.PostAsJsonAsync(Constant.RefreshTokenRoute, model);
+            string error = CheckResponseStatus(response);
+            if (!string.IsNullOrEmpty(error))
+                return new LoginResponse(false, error);
+            
+            var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new LoginResponse(false, ex.Message);
+        }
+    }
+}
