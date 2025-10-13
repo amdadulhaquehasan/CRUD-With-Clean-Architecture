11af648d605e4318714c5877e4d5284c88a77a58
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-10-13 20:49:01 +0600

    Add Service Files

---
+++ Application/Services/IAccountService.cs
@@ -0,0 +1,22 @@
+using Application.DTOs.Request.Account;
+using Application.DTOs.Response;
+using Application.DTOs.Response.Account;
+
+namespace Application.Services;
+
+public interface IAccountService
+{
+    Task CreateAdmin();
+    
+    Task<GeneralResponse> CreateAccountAsync(CreateAccountDTO model);
+    
+    Task<LoginResponse> LoginAccountAsync(LoginDTO model);
+    
+    Task<LoginResponse> RefreshTokenAsync(RefreshTokenDTO model);
+    
+    Task<GeneralResponse> CreateRoleAsync(CreateRoleDTO model);
+    
+    Task<IEnumerable<GetUsersWithRolesResponseDTO>>  GetUsersWithRolesAsync();
+    
+    Task<GeneralResponse> ChangeUserRoleAsync(ChangeUserRoleRequestDTO model);
+}
