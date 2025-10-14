1375398ef3277f6c44abf8637f303bb6cf8d0482
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-10-14 20:18:37 +0600

    Update Debugger, Dll, CsProj, Nugets and Cache Files

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f3a411fdb9d0ed7c88a902e96d199d0c3dc7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9278f58c5ad115b677866c741913fbd194453eb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
