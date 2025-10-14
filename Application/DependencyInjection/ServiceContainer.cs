341c3e7ae782eb0407691402769b0d7144529408
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-10-14 20:14:22 +0600

    Add ServiceContainer File

---
+++ Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,27 @@
+using Application.Extensions;
+using Application.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using NetcodeHub.Packages.Extensions.LocalStorage;
+
+namespace Application.DependencyInjection;
+
+public static class ServiceContainer
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services)
+    {
+        services.AddScoped<IAccountService, AccountService>();
+        services.AddAuthorizationCore();
+        services.AddNetcodeHubLocalStorageService();
+        services.AddScoped<Extensions.LocalStorageService>();
+        services.AddScoped<HttpClientService>();
+        services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+        services.AddScoped<CustomHttpHandler>();
+        services.AddCascadingAuthenticationState();
+        services.AddHttpClient("WebUIClient", client =>
+        {
+            client.BaseAddress = new Uri("https://localhost:7053");
+        }).AddHttpMessageHandler<CustomHttpHandler>();
+        return services;
+    }
+}
